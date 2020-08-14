f9b3e817a2a57c5872b925152380f618b74acc8a
ustfyplix1 <ustfyplix1@gmail.com>
2020-08-14 22:28:12 +0800

    ready for deploy

---
+++ src/apis/youtube.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+const KEY = "AIzaSyARVF--bjME2ET09Nz15Lhs1IKbofrpTjA";
+//why KEY not key  mean that KEY is constant value
+
+//does not have ending / of baseurl
+
+//asynchronous syntax
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResults: 5,
+    key: `${KEY}`
+  }
+});
